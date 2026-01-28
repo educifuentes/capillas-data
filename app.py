44e7dc332868d67d4820431cd0c195f8c7fe2c5c
educifuentes <edu.cifuentes@gmail.com>
2026-01-28 13:59:33 -0300

    feat - fct colectas

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 from utilities.load_sources import load_table_to_dataframe
+from models._fct_colectas import stg_colectas
 
 st.title("Capillas")
 
@@ -9,13 +10,23 @@ colecta_unitaria = load_table_to_dataframe("analisis_COLECTA_unitaria")
 comunas = load_table_to_dataframe("Cod_comuna")
 colectas_por_ano = load_table_to_dataframe("colectas_por_ano")
 
-st.subheader("sources")
 
-st.markdown("### colectas")
+st.header("Modelos", divider = True)
+
+
+fct_colectas = stg_colectas()
+st.dataframe(fct_colectas)
+
+
+### ---
+
+st.header("Fuentes", divider = True)
+
+st.subheader("colectas")
 st.dataframe(colecta_unitaria)
 
-st.markdown("### comunas")
+st.subheader("comunas")
 st.dataframe(comunas)
 
-st.markdown("### colectas por ano")
+st.subheader("colectas por ano")
 st.dataframe(colectas_por_ano)
--- models/_fct_colectas.py
+++ models/_fct_colectas.py
@@ -1,10 +1,31 @@
+import pandas as pd
 from models._dim_comunas import stg_cod_comunas
-
-
-
+from utilities.load_sources import load_table_to_dataframe
 
 def stg_colectas():
-        colectas_unitaria = load_table_to_dataframe('analisis_COLECTA_unitaria')
-        comunas_df = stg_cod_comunas()
- 
+    # Load sources
+    colectas_unitaria = load_table_to_dataframe('analisis_COLECTA_unitaria')
+    raw_comunas = load_table_to_dataframe('Cod_comuna')
+    
+    # Process comunas
+    comunas_df = stg_cod_comunas(raw_comunas)
+    
+    if colectas_unitaria is not None and comunas_df is not None:
+        # Perform left join
+        # Left key: 'colectas_unitaria', Right key: 'codigo_comuna'
+        df_joined = pd.merge(
+            colectas_unitaria, 
+            comunas_df, 
+            left_on='CodComuna', 
+            right_on='codigo_comuna', 
+            how='left'
+        )
+        return df_joined
+    
+    return None
 
+if __name__ == "__main__":
+    df_fct = stg_colectas()
+    if df_fct is not None:
+        print("Successfully merged colectas with comunas")
+        print(df_fct.head())
